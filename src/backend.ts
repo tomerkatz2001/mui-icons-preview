a48b8cd4beee8e992a642e711bd97827b27f9702
tomer katz <64071448+tomerkatz2001@users.noreply.github.com>
2024-10-21 23:27:27 +0300

    showing icon

---
+++ src/backend.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { getIconSVGByName } from './svg-creator';
+
+
+export async function getIconSVGAbsulotePath(icon:string, extensionPath:string)
+{
+    const iconSvg = getIconSVGByName(icon);
+    if(!iconSvg)
+    {
+        console.log(`could not file icon: [${icon}]`);
+        return null;
+    }
+   return await saveIconFile(iconSvg, icon, extensionPath);
+}
+
+async function saveIconFile(iconContent: string, iconName:string, extensionPath:string): Promise<string> {
+    const iconFileName = `${iconName}.svg`; // Define your icon file name
+    const iconFilePath = path.join(extensionPath, iconFileName); // Path where you want to save the file
+
+    // Write the content to the file
+    fs.writeFileSync(iconFilePath, iconContent, 'utf8');
+
+    return iconFilePath;
+}
---
+++ src/editor_scanning.ts
@@ -0,0 +1,29 @@
+
+export function getMUIIconsImports(fileContent: string)
+{
+    const lines = fileContent.split('\n');
+    const icons: string[] = [];
+    const importPattern = /import\s+(\w+)\s+from\s+['"]@mui\/icons-material\/(\w+)['"]/;
+
+    lines.forEach(line => {
+        const match = line.match(importPattern);
+        if (match) {
+            const iconName = match[2]; // Capture the icon name from the import path
+            icons.push(iconName);
+        }
+    });
+
+    return icons;
+}
+
+export function getLinesIcon(line: string, iconsBank: string[])
+{
+    for(const icon of iconsBank)
+    {
+        if (line.includes(icon))
+        {
+            return icon;
+        }
+    }
+    return null;
+}
